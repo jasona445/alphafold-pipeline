6746088ae5b1716127afef1d6da262d40c5f154d
Jason Aposotl <jga1@dslextreme.com>
2022-03-29 21:43:39 -0400

    small fix

--- scripts/batch-fold.sh
+++ scripts/batch-fold.sh
@@ -48,6 +48,7 @@ do
     echo "----Starting protein $curr----"
   else
     echo ">$curr\n$line" > tmp.fasta
+    echo $alphadir/run_alphafold.sh -d $datadir -o $outdir -f ./tmp.fasta -t $t -c $dbs
     source $alphadir/run_alphafold.sh -d $datadir -o $outdir -f ./tmp.fasta -t $t -c $dbs
     rm tmp.fasta
   fi
